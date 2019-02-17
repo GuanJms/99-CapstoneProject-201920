96ddd4204dd4c9f225b2a3ce30306d87ea68b2a8
guans <guans@rose-hulman.edu>
2019-02-17 01:26:25 -0500

    final project about going to position part updates

--- src/shengjun_final_project_on_laptop.py
+++ src/shengjun_final_project_on_laptop.py
@@ -34,7 +34,7 @@ def main():
     }
     mqtt_sender = com.MqttClient()
     mqtt_sender.connect_to_ev3()
-
+    frobot = shared_gui_delegate_on_robot.frobot()
     root = tkinter.Tk()
     root.title("Shengjun's CSSE 120 Final Project")
 
@@ -42,10 +42,9 @@ def main():
     main_frame.grid()
     main_label = ttk.Label(main_frame, text='Robot Running on Guitar Platform')
 
-    guitar_width_input_frame = guitar_width(main_frame, mqtt_sender)
+    guitar_width_input_frame = guitar_width(main_frame, mqtt_sender,frobot)
     last_button = None
-    frobot = shared_gui_delegate_on_robot.frobot()
-    note_frame = music_buttons(main_frame, mqtt_sender,frobot)
+    note_frame = music_buttons(main_frame, mqtt_sender,frobot,note_pics)
 
     # canvas part
     canvas = tkinter.Canvas(main_frame, width=480, height=520, bg='white')
@@ -58,7 +57,7 @@ def main():
     root.mainloop()
 
 
-def guitar_width(main_frame, mqtt_sender):
+def guitar_width(main_frame, mqtt_sender,frobot):
     guitar_width_input_frame = ttk.Frame(main_frame, padding=10, borderwidth=5, relief="groove")
     gitar_label = ttk.Label(guitar_width_input_frame, text="Guitar width(inches):")
     gitar_entry = ttk.Entry(guitar_width_input_frame, width=8)
@@ -69,7 +68,7 @@ def guitar_width(main_frame, mqtt_sender):
     start_generate.grid(row=0, column=2)
     # todo: Generate botton method
     start_generate["command"] = lambda: handle_start_generate(float(gitar_entry.get()) / 480, float(gitar_entry.get()),
-                                                              mqtt_sender)
+                                                              mqtt_sender,frobot)
     # k = float(gitar_entry.get()) / 480  # pic in data calculated for inches in actual
     return guitar_width_input_frame
 
@@ -89,7 +88,7 @@ def guitar_canvas(canvas):
     canvas.create_line(0, 150, 480, 40)
 
 
-def music_buttons(main_frame, mqtt_sender,frobot):
+def music_buttons(main_frame, mqtt_sender,frobot,note_pics):
     music_button_frame = ttk.Frame(main_frame, padding=10, borderwidth=5, relief="groove")
     lista = ['C', 'D', 'E', 'F', 'G', 'A', 'B']
     for i in range(3, 6, 1):
@@ -98,19 +97,20 @@ def music_buttons(main_frame, mqtt_sender,frobot):
             if button_name != 'C3' and button_name != 'D3' and button_name != 'A5' and button_name != 'B5':
                 position_x = i - 3
                 position_y = j
-                button_construct(position_x, position_y, music_button_frame, mqtt_sender, button_name,frobot)
+                button_construct(position_x, position_y, music_button_frame, mqtt_sender, button_name,frobot,note_pics)
     return music_button_frame
 
 
-def button_construct(position_x, position_y, music_button_frame, mqtt_sender, button_name, frobot):
+def button_construct(position_x, position_y, music_button_frame, mqtt_sender, button_name, frobot,note_pics):
     button = ttk.Button(music_button_frame, text=button_name)
     button.grid(row=position_y, column=position_x)
-    button["command"] = lambda: handle_note(button_name, mqtt_sender, frobot)
+    button["command"] = lambda: handle_note(button_name, mqtt_sender, frobot,note_pics)
     button.grid(row=position_y, column=position_x)
 
 
-def handle_note(button_name, mqtt_sender, frobot):
+def handle_note(button_name, mqtt_sender, frobot, note_pics):
     clockwise, degree, distance = move(frobot.last_button,frobot.button_name,note_pics, frobot.k)
+    print(frobot.last_button,frobot.button_name,note_pics, frobot.k)
     print(clockwise, degree, distance)
     mqtt_sender.send_message("note", [clockwise, degree, distance])
 
@@ -118,9 +118,12 @@ def handle_note(button_name, mqtt_sender, frobot):
 
 
 
-def handle_start_generate(k, inches, mqtt_sender):
+def handle_start_generate(k, inches, mqtt_sender, frobot):
     print('Start generate guitar')
     print('Send rate to robot')
+    frobot.k = k
+    frobot.last_button = "E3"
+    frobot.inches = inches
     mqtt_sender.send_message("generate", [k, inches])
 
 def move(last_button, button_name, note_pics, k):
