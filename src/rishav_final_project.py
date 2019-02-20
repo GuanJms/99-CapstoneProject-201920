08070b75d5f344f973c1c0a595f6718e74d25f06
khoslarr <khoslarr@rose-hulman.edu>
2019-02-20 11:35:21 -0500

    Added final project file

---
+++ src/rishav_final_project.py
@@ -0,0 +1,15 @@
+"""
+  Capstone Project.  Code to run on a LAPTOP (NOT the robot).
+  Displays the Graphical User Interface (GUI) and communicates with the robot.
+
+  Authors:  Your professors (for the framework)
+    and Shengjun Guan.
+  Winter term, 2018-2019.
+"""
+
+import mqtt_remote_method_calls as com
+import tkinter
+from tkinter import ttk
+import shared_gui
+import time
+
